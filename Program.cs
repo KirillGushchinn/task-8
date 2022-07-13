3067d095d9a9abb7284fab437e12b2717c4864d3
Kirill <krl.gushchinn@mail.ru>
2022-07-13 08:18:23 -0700

    Готовое задание 8

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Zadanie8
+{
+    public static void Main(string[] args)
+    {
+        int N,i;
+        i=1;
+        Console.Write ("Введите число N=");
+        N=int.Parse(Console.ReadLine());
+        Console.Write("N=");
+        while (i<N){
+         if (i%2==0){
+             Console.Write($"{i} ");
+         }   
+             i++;
+        }
+    }
+}
